30690c383ed345aa1654f6663886f1095ee4ec50
Pam Rucinque <pamela.rucinque@gmail.com>
2016-10-18 18:52:13 +1100

    I like English

--- handler.js
+++ handler.js
@@ -3,5 +3,5 @@
 // Your first function handler
 module.exports.hello = (event, context, cb) => {
   console.log('*******', JSON.stringify(context));
-  cb(null, { message: 'I\'m greeting endpoint, hello there!', event });
+  cb(null, { message: 'I\'m a greeting endpoint, hello there!', event });
 };
