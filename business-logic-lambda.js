1fed7b8b96bbffbaddec242b61e7d0d8e4c024e5
Pam Rucinque <pamela.rucinque@gmail.com>
2016-10-19 14:46:25 +1100

    renaming the business logic lambda

--- business-logic-lambda.js
+++ business-logic-lambda.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Your first function handler
-module.exports.hello = (event, context, cb) => {
+module.exports.handler = (event, context, cb) => {
   console.log('*******', JSON.stringify(context));
   cb(null, { message: 'I\'m a greeting endpoint, hello there!', event });
 };
